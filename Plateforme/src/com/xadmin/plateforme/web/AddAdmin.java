d5c8aaa12157503b4e97b176642926dbacb2f901
unknown <chqirsamia@gmail.com>
2021-02-17 22:24:49 +0100

    update admin

--- Plateforme/src/com/xadmin/plateforme/web/AddAdmin.java
+++ Plateforme/src/com/xadmin/plateforme/web/AddAdmin.java
@@ -1,6 +1,10 @@
 package com.xadmin.plateforme.web;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -108,7 +112,7 @@ public class AddAdmin extends HttpServlet {
 		String password =request.getParameter("password");
 		
 		User newUser = new User(name,prenom, email,sexe, tel,password,"admin");
-		
+		traiterMotDePasse(password, newUser);
 		userDao.insertAdmin(newUser);
 		response.sendRedirect("list");
 	}
@@ -125,6 +129,7 @@ public class AddAdmin extends HttpServlet {
 		//String role =request.getParameter("role");
 
 		User Admin = new User(id,name,prenom, email,sexe, tel,password,"admin");
+		traiterMotDePasse(password, Admin);
 		userDao.updateUser(Admin);
 		response.sendRedirect("list");
 	}
@@ -136,5 +141,43 @@ public class AddAdmin extends HttpServlet {
 		response.sendRedirect("list");
 
 	}
+	public static byte[] getSHA(String input) throws NoSuchAlgorithmException 
+    {  
+        // Static getInstance method is called with hashing SHA  
+        MessageDigest md = MessageDigest.getInstance("SHA-256");  
+  
+        // digest() method called  
+        // to calculate message digest of an input  
+        // and return array of byte 
+        return md.digest(input.getBytes(StandardCharsets.UTF_8));  
+    } 
+    
+    public static String toHexString(byte[] hash) 
+    { 
+        // Convert byte array into signum representation  
+        BigInteger number = new BigInteger(1, hash);  
+  
+        // Convert message digest into hex value  
+        StringBuilder hexString = new StringBuilder(number.toString(16));  
+  
+        // Pad with leading zeros 
+        while (hexString.length() < 32)  
+        {  
+            hexString.insert(0, '0');  
+        }  
+  
+        return hexString.toString();  
+    } 
+    private void traiterMotDePasse(String motDePasse,User user){
+        
+	        try {
+				user.setPassword( toHexString(getSHA(motDePasse))) ;
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+        
+    }
 
 }
